9225ad7caf392341c856cdbb35125464eda2e6ce
Angelo Pirola <angelo9871@gmail.com>
2023-02-16 19:10:21 +0100

    Aggiunto endpoint DELETE

---
+++ src/SampleAPI.FastEndpoints/Controllers/DeletePerson/DeletePersonEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using SampleAPI.FastEndpoints.Infrastructure;
+
+namespace SampleAPI.FastEndpoints.Controllers.DeletePerson;
+
+public class DeletePersonEndpoint : Endpoint<DeletePersonRequest>
+{
+    private readonly DataDbContext dbContext;
+
+    public DeletePersonEndpoint(DataDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public override void Configure()
+    {
+        Delete("/api/people/{id}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(DeletePersonRequest req, CancellationToken ct)
+    {
+        var user = await dbContext.People.FirstOrDefaultAsync(x => x.Id == req.Id);
+
+        if (user == null)
+        {
+            await SendAsync(null, 404, ct);
+        }
+        else
+        {
+            dbContext.People.Remove(user);
+            await dbContext.SaveChangesAsync(ct);
+
+            await SendAsync(null, 200, ct);
+        }
+    }
+}
---
+++ src/SampleAPI.FastEndpoints/Controllers/DeletePerson/DeletePersonRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.FastEndpoints.Controllers.DeletePerson;
+
+public class DeletePersonRequest
+{
+    [FromBody]
+    public int Id { get; set; }
+}
